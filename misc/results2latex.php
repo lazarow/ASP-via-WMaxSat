21c3e237ff8e7cbccefc95a0accb2b0968805d9a
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2024-01-24 00:16:20 +0100

    update the results

--- misc/results2latex.php
+++ misc/results2latex.php
@@ -1,20 +1,24 @@
 <?php
 $data = <<<'DATA'
-p1	60	1500	731	25,469	TL	6,114*	43,101
-p2	50	1000	454	2,904	TL	1,024*	8,689
-p3	40	700	737	0,146	5,492	0,168*	0,704
-p4	40	600	297	0,215	6,423	0,17*	1,874
-p5	75	2000	354	97,145	TL	9,154*	75,854
-p6	80	2200	397	44,584	TL	10,335*	72,938
-p7	80	2100	289	38,46	TL	9,708*	42,465
-p8	90	2100	178	13,403	TL	6,146*	26,596
-p9	75	2000	331	95,404	TL	8,984*	78,422
-p10	80	2000	252	30,106	TL	7,416*	46,408
+p1	75	9	15	69,763	TL	TL	58,944*
+p2	75	9	15	67,295	TL	TL	42,117*
+p3	75	9	15	47,85*	TL	TL	60,7
+p4	75	9	15	52,795*	TL	TL	58,55
+p5	75	9	15	50,324*	TL	TL	60,635
+p6	75	9	15	68,729	TL	TL	48,358*
+p7	75	9	15	30,755*	TL	TL	55
+p8	75	9	15	53,189*	TL	TL	68,786
+p9	75	9	15	48,655*	TL	TL	52,672
+p10	75	9	15	91,471	TL	TL	54,626*
 DATA;
+
+$skip = 3;
 
 $data = array_map('trim', explode("\n", trim($data)));
 $firstRow = preg_split('/\s+/', $data[0]);
 $total = array_fill(0, count($firstRow), 0);
+$successes = array_fill(0, count($firstRow), 0);
+$nofInstances = count($data);
 
 for ($i = 0; $i < count($data); ++$i) {
     $row = preg_split('/\s+/', $data[$i]);
@@ -26,6 +30,7 @@ for ($i = 0; $i < count($data); ++$i) {
             if (strpos($row[$j], '*') !== false) {
                 $value = (float) strtr(mb_substr($row[$j], 0, -1), [',' => '.']);
                 $total[$j] += $value;
+                $successes[$j]++;
                 echo '$ \\mathbf{' . number_format(
                     $value,
                     2,
@@ -35,6 +40,7 @@ for ($i = 0; $i < count($data); ++$i) {
             } else {
                 $value = (float) strtr($row[$j], [',' => '.']);
                 $total[$j] += $value;
+                $successes[$j]++;
                 echo '$ ' . number_format(
                     (float) strtr($row[$j], [',' => '.']),
                     2,
@@ -44,6 +50,7 @@ for ($i = 0; $i < count($data); ++$i) {
             }
         } else if (is_numeric($row[$j])) {
             $total[$j] += (int) $row[$j];
+            $successes[$j]++;
             echo '$ ' . ((int) $row[$j]) . ' $';
         } else if ($row[$j] === 'Tak') {
             echo 'Yes';
@@ -62,11 +69,47 @@ for ($i = 0; $i < count($data); ++$i) {
 echo "\midrule\n";
 echo 'Total';
 for ($i = 1; $i < count($total); ++$i) {
+    if ($i <= $skip) {
+        echo ' & ';
+        continue;
+    }
     echo ' & $ ' . number_format(
         $total[$i],
         2,
         '.',
         ''
     ) . ' $ \unit{\second}';
+}
+echo " \\\\ \n";
+
+echo "\midrule\n";
+echo 'Average';
+for ($i = 1; $i < count($total); ++$i) {
+    if ($i <= $skip) {
+        echo ' & ';
+        continue;
+    }
+    echo ' & $ ' . number_format(
+        $total[$i] / $nofInstances,
+        2,
+        '.',
+        ''
+    ) . ' $ \unit{\second}';
+}
+echo " \\\\ \n";
+
+echo "\midrule\n";
+echo 'Success Rate';
+for ($i = 1; $i < count($total); ++$i) {
+    if ($i <= $skip) {
+        echo ' & ';
+        continue;
+    }
+    echo ' & $ ' . number_format(
+        ($successes[$i] / $nofInstances) * 100,
+        0,
+        '.',
+        ''
+    ) . '\% $';
 }
 echo " \\\\ \n";
